398744e11d18060921f6403620a90fccfc80385b
unknown <pupan@chinafintech.cn>
2022-09-02 15:23:11 +0800

    small fix

--- lib_pds/src/main/java/com/wiseasy/pds/response/DeviceInitResponse.java
+++ lib_pds/src/main/java/com/wiseasy/pds/response/DeviceInitResponse.java
@@ -1,6 +1,6 @@
 package com.wiseasy.pds.response;
 
-import org.json.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.List;
 
@@ -13,12 +13,12 @@ import java.util.List;
  * @author pupan
  */
 public class DeviceInitResponse extends BaseResponse {
-    BaseInfo basic_info;
-    String pay_info;
+    JSONObject basic_info;
+    List<JSONObject> pay_info;
     String aes_key_cipher;
     String aes_key_expire_time;
 
-    public String getPay_info() {
+    public List<JSONObject> getPay_info() {
         return pay_info;
     }
 
@@ -30,11 +30,11 @@ public class DeviceInitResponse extends BaseResponse {
         return aes_key_expire_time;
     }
 
-    public BaseInfo getBasic_info() {
+    public JSONObject getBasic_info() {
         return basic_info;
     }
 
-    public void setBasic_info(BaseInfo basic_info) {
+    public void setBasic_info(JSONObject basic_info) {
         this.basic_info = basic_info;
     }
 
@@ -47,204 +47,8 @@ public class DeviceInitResponse extends BaseResponse {
         this.aes_key_expire_time = aes_key_expire_time;
     }
 
-    public void setPay_info(String pay_info) {
+    public void setPay_info(List<JSONObject> pay_info) {
         this.pay_info = pay_info;
     }
-
-    /**
-     * 基础配置信息
-     */
-    public class BaseInfo {
-        String country;
-        String merchant_no;
-        String institution_no;
-        String city;
-        String store_no;
-        String timezone;
-        String merchant_name;
-        String language;
-        int merchant_status;
-        List<String> store_photos;
-        String institution_name;
-        String address_detail;
-        String country_no;
-        String country_code;
-        JSONObject configuration_parameters;
-        String country_code2;
-        String supported_settlement_currencies;
-        String store_name;
-        String supported_price_currencies;
-        String app_id;
-        String terminal_sn;
-
-        public int getMerchant_status() {
-            return merchant_status;
-        }
-
-        public JSONObject getConfiguration_parameters() {
-            return configuration_parameters;
-        }
-
-        public List<String> getStore_photos() {
-            return store_photos;
-        }
-
-        public String getAddress_detail() {
-            return address_detail;
-        }
-
-        public String getSupported_price_currencies() {
-            return supported_price_currencies;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public String getCountry_code() {
-            return country_code;
-        }
-
-        public String getCountry_code2() {
-            return country_code2;
-        }
-
-        public String getCountry_no() {
-            return country_no;
-        }
-
-        public String getInstitution_name() {
-            return institution_name;
-        }
-
-        public String getApp_id() {
-            return app_id;
-        }
-
-        public String getTerminal_sn() {
-            return terminal_sn;
-        }
-
-        public String getInstitution_no() {
-            return institution_no;
-        }
-
-        public String getLanguage() {
-            return language;
-        }
-
-        public String getMerchant_name() {
-            return merchant_name;
-        }
-
-        public String getMerchant_no() {
-            return merchant_no;
-        }
-
-        public String getStore_name() {
-            return store_name;
-        }
-
-        public String getStore_no() {
-            return store_no;
-        }
-
-        public String getSupported_settlement_currencies() {
-            return supported_settlement_currencies;
-        }
-
-        public String getTimezone() {
-            return timezone;
-        }
-
-        public void setTerminal_sn(String terminal_sn) {
-            this.terminal_sn = terminal_sn;
-        }
-
-        public void setAddress_detail(String address_detail) {
-            this.address_detail = address_detail;
-        }
-
-        public void setApp_id(String app_id) {
-            this.app_id = app_id;
-        }
-
-        public void setCity(String city) {
-            this.city = city;
-        }
-
-        public void setConfiguration_parameters(JSONObject configuration_parameters) {
-            this.configuration_parameters = configuration_parameters;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
-
-        public void setCountry_code(String country_code) {
-            this.country_code = country_code;
-        }
-
-        public void setCountry_code2(String country_code2) {
-            this.country_code2 = country_code2;
-        }
-
-        public void setCountry_no(String country_no) {
-            this.country_no = country_no;
-        }
-
-        public void setInstitution_name(String institution_name) {
-            this.institution_name = institution_name;
-        }
-
-        public void setInstitution_no(String institution_no) {
-            this.institution_no = institution_no;
-        }
-
-        public void setLanguage(String language) {
-            this.language = language;
-        }
-
-        public void setMerchant_name(String merchant_name) {
-            this.merchant_name = merchant_name;
-        }
-
-        public void setMerchant_no(String merchant_no) {
-            this.merchant_no = merchant_no;
-        }
-
-        public void setMerchant_status(int merchant_status) {
-            this.merchant_status = merchant_status;
-        }
-
-        public void setStore_name(String store_name) {
-            this.store_name = store_name;
-        }
-
-        public void setStore_no(String store_no) {
-            this.store_no = store_no;
-        }
-
-        public void setStore_photos(List<String> store_photos) {
-            this.store_photos = store_photos;
-        }
-
-        public void setSupported_price_currencies(String supported_price_currencies) {
-            this.supported_price_currencies = supported_price_currencies;
-        }
-
-        public void setSupported_settlement_currencies(String supported_settlement_currencies) {
-            this.supported_settlement_currencies = supported_settlement_currencies;
-        }
-
-        public void setTimezone(String timezone) {
-            this.timezone = timezone;
-        }
-    }
-
 }
 
--- lib_pds/src/main/java/com/wiseasy/pds/response/TransactionListQueryResponse.java
+++ lib_pds/src/main/java/com/wiseasy/pds/response/TransactionListQueryResponse.java
@@ -1,5 +1,7 @@
 package com.wiseasy.pds.response;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.List;
 
 /**
@@ -7,127 +9,26 @@ import java.util.List;
  * User: pupan
  * Date: 8/16/2022
  * Time: 5:55 PM
+ *
  * @author pupan
  */
 public class TransactionListQueryResponse extends BaseResponse {
     int total;
-    List<TransactionDetail> list;
+    List<JSONObject> list;
 
     public int getTotal() {
         return total;
     }
 
-    public List<TransactionDetail> getList() {
+    public List<JSONObject> getList() {
         return list;
     }
 
-    public void setList(List<TransactionDetail> list) {
+    public void setList(List<JSONObject> list) {
         this.list = list;
     }
 
     public void setTotal(int total) {
         this.total = total;
-    }
-}
-
-class TransactionDetail {
-    String trans_no;
-    double trans_amount;
-    double order_amount;
-    int trans_status;
-    int trans_type;
-    String pay_method_id;
-    String pay_method_name;
-    String pay_method_icon_url;
-    String pay_scenario;
-    String created_time;
-    String trans_end_time;
-
-    public String getTrans_end_time() {
-        return trans_end_time;
-    }
-
-    public String getTrans_no() {
-        return trans_no;
-    }
-
-    public double getTrans_amount() {
-        return trans_amount;
-    }
-
-    public int getTrans_status() {
-        return trans_status;
-    }
-
-    public String getCreated_time() {
-        return created_time;
-    }
-
-    public String getPay_method_icon_url() {
-        return pay_method_icon_url;
-    }
-
-    public String getPay_method_id() {
-        return pay_method_id;
-    }
-
-    public double getOrder_amount() {
-        return order_amount;
-    }
-
-    public int getTrans_type() {
-        return trans_type;
-    }
-
-    public String getPay_method_name() {
-        return pay_method_name;
-    }
-
-    public String getPay_scenario() {
-        return pay_scenario;
-    }
-
-    public void setTrans_status(int trans_status) {
-        this.trans_status = trans_status;
-    }
-
-    public void setTrans_no(String trans_no) {
-        this.trans_no = trans_no;
-    }
-
-    public void setTrans_amount(double trans_amount) {
-        this.trans_amount = trans_amount;
-    }
-
-    public void setTrans_end_time(String trans_end_time) {
-        this.trans_end_time = trans_end_time;
-    }
-
-    public void setOrder_amount(double order_amount) {
-        this.order_amount = order_amount;
-    }
-
-    public void setPay_method_id(String pay_method_id) {
-        this.pay_method_id = pay_method_id;
-    }
-
-    public void setTrans_type(int trans_type) {
-        this.trans_type = trans_type;
-    }
-
-    public void setCreated_time(String created_time) {
-        this.created_time = created_time;
-    }
-
-    public void setPay_method_icon_url(String pay_method_icon_url) {
-        this.pay_method_icon_url = pay_method_icon_url;
-    }
-
-    public void setPay_method_name(String pay_method_name) {
-        this.pay_method_name = pay_method_name;
-    }
-
-    public void setPay_scenario(String pay_scenario) {
-        this.pay_scenario = pay_scenario;
     }
 }
