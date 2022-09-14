1f601ba4c986264ba121005743b4b9cc321fc71c
unknown <pupan@chinafintech.cn>
2022-09-14 11:12:10 +0800

    small fix

---
+++ lib_pds/src/main/java/com/wiseasy/pds/response/TransFileUpLoadResponse.java
@@ -0,0 +1,19 @@
+package com.wiseasy.pds.response;
+
+/**
+ * Created by Android Studio.
+ * User: pupan
+ * Date: 9/14/2022
+ * Time: 11:11 AM
+ */
+class TransFileUpLoadResponse {
+    String file_key;
+
+    public String getFile_key() {
+        return file_key;
+    }
+
+    public void setFile_key(String file_key) {
+        this.file_key = file_key;
+    }
+}
